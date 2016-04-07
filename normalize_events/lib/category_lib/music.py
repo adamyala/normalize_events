b14ea61739c5efcb4023ddbab4dc2180674550f2
Adam Yala <adam@adamyala.com>
2016-04-07 09:59:51 -0500

    fix Other logic in music category

--- normalize_events/lib/category_lib/music.py
+++ normalize_events/lib/category_lib/music.py
@@ -4,6 +4,6 @@ import genres
 class Music(object):
     def get_categories_for_insert(self, event_string):
         result = genres.find(event_string)
-        if result == []:
+        if not result:
             return ['Other']
         return result
