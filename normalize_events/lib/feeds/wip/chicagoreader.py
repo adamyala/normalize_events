8322e36536124b11bb85795056acacf140749baa
Adam Yala <adamy@5thcolumn.net>
2016-07-08 16:44:27 -0500

    comment out WIP files so they stop blowing up my IDE

--- normalize_events/lib/feeds/wip/chicagoreader.py
+++ normalize_events/lib/feeds/wip/chicagoreader.py
@@ -1,11 +1,11 @@
-links and name
-#event16678460 > div.listing > h3 > a
-
-descrip
-.descripTxt
-
-date
-div.listing > h3
-
-place
-div.listingLocation > a:nth-child(1)
+# links and name
+# #event16678460 > div.listing > h3 > a
+#
+# descrip
+# .descripTxt
+#
+# date
+# div.listing > h3
+#
+# place
+# div.listingLocation > a:nth-child(1)
